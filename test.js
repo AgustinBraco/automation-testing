2fff0238a02ef60a96ab3376c05636a86b7a3ccc
Agustin <bracoagustin@gmail.com>
2023-01-26 12:45:02 -0300

    Simple test

--- test.js
+++ test.js
@@ -4,10 +4,17 @@ fixture `Empezando con TestCafe`
     .page `https://www.google.com`;
 
 test('Búsqueda en Google', async t => {
-    const searchField = Selector('input.gLFyf')
+    // Selector('ETIQUETA.CLASE') el punto significa class
+    // Si hay varios con la misma clase va a tomar el primero
+    const searchField = Selector('input.gLFyf');
+    const ourHistory = Selector('h3.LC20lb.MBeuO.DKV0Md');
 
+    // Lo tenemos por defecto.
     await t
-    
-    .typeText(searchField, 'test')
+
+    // Tipear texto en (WEBELEMENT, 'TEXTO')
+    .typeText(searchField, 'avature')
         .pressKey('enter')
+        // Presionar tecla ENTER
+    .click(ourHistory)
 });

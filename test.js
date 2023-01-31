09f5d8ad0b79cb4b0b80e66b15aadbf137a6bb25
Agustin <bracoagustin@gmail.com>
2023-01-31 13:58:44 -0300

    Form validation

--- test.js
+++ test.js
@@ -1,19 +1,20 @@
 import {Selector} from 'testcafe';
 
-fixture `Empezando con TestCafe`
-.page `https://www.google.com`;
+fixture `Starting with TestCafe`
+    .page `https://www.google.com`;
 
 const googleSearch = Selector("input.gLFyf");
 const avatureLink = Selector("h3.LC20lb.MBeuO.DKV0Md");
-const careersLink = Selector("#menu-item-41598");
-const avatureSearch = Selector("input.TextField.WizardFieldInputContainer.WizardFieldInput");
+const contactLink = Selector("#menu-item-27727");
+const submitButton = Selector("#enContactUs");
+const validationText = Selector("div.ffErrorVal.fw400.invalid-feedback");
 
-test('Búsqueda en Google', async t => {
+test('Avature test', async t => {
     await t
     .typeText(googleSearch, 'avature')
     .pressKey('enter')
     .click(avatureLink)
-    .click(careersLink)
-    .typeText(avatureSearch, 'QA')
-    .pressKey('enter')
+    .click(contactLink)
+    .click(submitButton)
+    .expect(validationText.innerText).contains('Please enter your first name')
 });

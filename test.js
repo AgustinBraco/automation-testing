9a6478df8a4c916a5be0c44a94bc4f64921d4de0
Agustin <bracoagustin@gmail.com>
2023-02-01 13:52:38 -0300

    TestSpeed and Screenshot

--- test.js
+++ test.js
@@ -4,17 +4,21 @@ fixture `Starting with TestCafe`
     .page `https://www.google.com`;
 
 const googleSearch = Selector("input.gLFyf");
-const avatureLink = Selector("h3.LC20lb.MBeuO.DKV0Md");
+const avatureLink = Selector("h3.LC20lb");
+const cookiesButton = Selector("#onetrust-accept-btn-handler");
 const contactLink = Selector("#menu-item-27727");
 const submitButton = Selector("#enContactUs");
-const validationText = Selector("div.ffErrorVal.fw400.invalid-feedback");
+const validationText = Selector("div.ffErrorVal");
 
 test('Avature test', async t => {
     await t
+    .setTestSpeed(0.1)
     .typeText(googleSearch, 'avature')
     .pressKey('enter')
     .click(avatureLink)
+    .click(cookiesButton)
     .click(contactLink)
     .click(submitButton)
+    .takeScreenshot ({ fullPage: true})
     .expect(validationText.innerText).contains('Please enter your first name')
 });
